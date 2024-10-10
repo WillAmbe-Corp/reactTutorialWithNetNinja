db3fa0405276e7447ff9802e6e6bcd3e4951280f
WillAmbe-Corp <nebaprincewill237@gmail.com>
2024-10-10 22:00:36 +0100

    testing my understanding of custom hooks

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,22 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-const useFetch = (url) => {
+const useFetch = url => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    useEffect(() => {
+
+    useEffect( () => {
         setTimeout(() => {
             fetch(url)
-                .then(res => {
-                    if(!res.ok){
-                        throw Error("data unreachable");
-                    };
-                   return res.json()})
-                .then(data => setData(data))
-                .catch(err => setError(err.message))
-                setLoading(false);
-            }, 1000)
-    }, [url]);
+            .then(res => {
+                if(!res.ok){
+                    throw Error("data unreachable")
+                }
+                return res.json()})
+            .then(data => setData(data))
+            .catch(err => setError(err.message))
+            setLoading(false);
+        }, 1000)
+    }, [url])
     return {data, loading, error};
 }
 export default useFetch;
