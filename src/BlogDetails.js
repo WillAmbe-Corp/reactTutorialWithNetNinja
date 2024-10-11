9e8e00dbf6aea2343763f558776165f32dff0b01
WillAmbe-Corp <nebaprincewill237@gmail.com>
2024-10-11 23:41:46 +0100

    finalization of the project and addition of a 404 error page, enable delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,21 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
+import { useState } from "react";
 
 const BloDetails = () => {
+    const history = useHistory();
     const { id } = useParams();
+    const [deleting, setDeleting] = useState(false);
     const {data: blog, loading, error} = useFetch("http://localhost:8000/blogs/" + id);
+    
+    const handleDelete = () => {
+        fetch("http://localhost:8000/blogs/" + blog.id,{
+            method: "DELETE",
+        }).then(() =>{
+            history.push("/");
+        })
+        setDeleting(false);
+    }
     return (
         <div className="blog-details">
             <h1>Blog Details - {id}</h1>
@@ -12,8 +24,10 @@ const BloDetails = () => {
             {blog && 
             (<article>
                 <h2>{blog.title}</h2>
-                <p>Edited by {blog.author}</p>
+                <p>Edited by <span style={{fontWeight: 'bolder', fontSize: '20px' }}>{blog.author}</span></p>
                 <div>{blog.body}</div>
+                {!deleting && <button onClick={handleDelete}>Delete Blog</button>}
+                {deleting && <div>Deleting Blog...</div>}
             </article>)}
         </div>
     );
--- src/CreateBlog.js
+++ src/CreateBlog.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 const CreateBlog = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Will A.');
+    const [loading, setLoading] = useState(false);
+    const history = useHistory();
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = {title, author, body};
+        
+        fetch('http://localhost:8000/blogs',{
+            method:'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(blog)
+        })
+        .then(() => console.log("New Blog Added"))
+        setLoading(false);
+        history.push("/");
+        setTitle({title:""})
+        setAuthor({author:''});
+        setBody({body:""});
+    }
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title:</label>
+                <input type="text" required value={title} onChange={ e => setTitle(e.target.value)}/>
+                <label>Body:</label>
+                <textarea required value={body} onChange={e => setBody(e.target.value)}></textarea>
+                <label>Author:</label>
+                <select value={author} onChange={e => setAuthor(e.target.value)} >
+                    <option value="Will A."> Will A.</option>
+                    <option value="Ambe"> Ambe</option>
+                    <option value="Light Yagami"> Light Yagami.</option>
+                    <option value="Eren Yeager"> Eren Yeager</option>
+                    <option value="John Doe"> John Doe</option>
+                </select>
+                {!loading && <button>Add Blog</button>}
+                {loading && <button disabled>Blog Adding...</button>}
+            </form>
         </div>
     );
 }
